8c4b0feab5780300e35128971c67724530dea084
Head Gardener <trashbin2019np@gmail.com>
2024-11-11 07:43:16 +0300

    feat: bee2 example

---
+++ crypto/crypto.go
@@ -0,0 +1,27 @@
+package crypto
+
+// #cgo LDFLAGS: -lbee2
+// #include <stdlib.h>
+// #include <bee2/crypto/belt.h>
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func Encrypt(str []byte) ([]byte, error) {
+	buf := C.malloc(4096)
+	defer C.free(unsafe.Pointer(buf))
+	out := make([]byte, C.sizeof_octet*32)
+
+	fmt.Printf("%x\n", out)
+
+	C.beltHashStart(buf)
+	C.beltHashStepH(unsafe.Pointer(&str[0]), 0, buf)
+	C.beltHashStepG((*C.octet)(unsafe.Pointer(&out[0])), buf)
+
+	fmt.Printf("%x\n", out)
+
+	return out, nil
+}
---
+++ crypto/crypto_test.go
@@ -0,0 +1,17 @@
+package crypto
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+func TestEncrypt(t *testing.T) {
+	input := []byte("🙏🙏🙏")
+	want := regexp.MustCompile("eb6ba8bde3821909b63e14764485530fd8e875a23834d41d6c100ac446828c7e")
+	msg, err := Encrypt(input)
+	res := fmt.Sprintf("%x", msg)
+	if !want.MatchString(res) || err != nil {
+		t.Fatalf("%q, %v, not %#q", res, err, want)
+	}
+}
