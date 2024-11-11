1fc87de0314230d854482a492bfd97f779d9b76a
Head Gardener <trashbin2019np@gmail.com>
2024-11-11 04:47:57 +0300

    feat: bee2 build defenition

--- flake.nix
+++ flake.nix
@@ -11,6 +11,22 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [ go gopls gotools go-tools ];
         };
+
+        packages.bee2 = with pkgs; stdenv.mkDerivation {
+          pname = "bee2";
+          version = "v2.1.4";
+
+          src = fetchFromGitHub {
+            owner = "agievich";
+            repo = "bee2";
+            rev = "e0ea53134ff0939857de3e3ead72fa2a5318c6a4";
+            hash = "sha256-Fp2mCUknMD7z4WUkzL0E1FaNtW6WVDU2X94+DegRSFc=";
+          };
+
+          nativeBuildInputs = [ cmake ];
+
+          doCheck = true;
+        };
       };
     };
 }
