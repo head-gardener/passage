ae37bd0931efeae5fdf527f87486acdcd6b59549
Head Gardener <trashbin2019np@gmail.com>
2024-11-17 09:49:35 +0300

    feat(crypto): CTR

---
+++ pkg/crypto/ctr.go
@@ -0,0 +1,40 @@
+package crypto
+
+// #cgo LDFLAGS: -lbee2
+// #include <stdlib.h>
+// #include <bee2/crypto/belt.h>
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+// Belt counter encryption via bee2.
+func CTR(
+	out []byte,
+	src []byte,
+	key BeltKey,
+	iv BeltIV,
+	opt *CommonOpt,
+) (err error) {
+	var srcLen int
+	if opt != nil && opt.srcLen != 0 {
+		srcLen = opt.srcLen
+	} else {
+		srcLen = len(src)
+	}
+
+	ret := C.beltCTR(
+		unsafe.Pointer(&out[0]),
+		unsafe.Pointer(&src[0]),
+		(C.size_t)(srcLen),
+		(*C.octet)(unsafe.Pointer(&key[0])),
+		32,
+		(*C.octet)(unsafe.Pointer(&iv[0])),
+	)
+	if ret != 0 {
+		return fmt.Errorf("non-zero return: %v", ret)
+	}
+	return nil
+}
---
+++ pkg/crypto/ctr_test.go
@@ -0,0 +1,60 @@
+package crypto
+
+import (
+	"bytes"
+	"testing"
+	"testing/quick"
+)
+
+var propCTRIdentity = makeCryptoIdentity(nil, CTR, CTR)
+
+func TestCTR(t *testing.T) {
+	cases := []struct {
+		input string
+		key   string
+		iv    string
+		want  string
+	}{
+		{
+			input: "B194BAC80A08F53B366D008E584A5DE48504FA9D1BB6C7AC252E72C202FDCE0D5BE3D61217B96181FE6786AD716B890B",
+			key:   "E9DEE72C8F0C0FA62DDB49F46F73964706075316ED247A3739CBA38303A98BF6",
+			iv:    "BE32971343FC9A48A02A885F194B09A1",
+			want:  "52C9AF96FF50F64435FC43DEF56BD797D5B5B1FF79FB41257AB9CDF6E63E81F8F00341473EAE409833622DE05213773A",
+		},
+	}
+	for i := range cases {
+		input := mustDecode(t, cases[i].input)
+		key := mustBeKey(t, cases[i].key)
+		iv := mustBeIV(t, cases[i].iv)
+		enc := make([]byte, len(input))
+		dec := make([]byte, len(input))
+
+		propCTRIdentity(t, input, key, iv, enc, dec)
+
+		want := mustDecode(t, cases[i].want)
+		if !bytes.Equal(enc, want) {
+			t.Fatalf("no match:\n%x + %x ->\n%x, not\n%x", input, key, enc, want)
+		}
+	}
+}
+
+func TestCTRProp(t *testing.T) {
+	encBuf := make([]byte, maxSize)
+	decBuf := make([]byte, maxSize)
+
+	f := func(input []byte, pass []byte) (ok bool) {
+		key := mustDerive(t, pass)
+		iv := mustContainIV(t, encBuf)
+		enc := encBuf[:len(input)]
+		dec := decBuf[:len(input)]
+
+		propCTRIdentity(t, input, key, iv, enc, dec)
+
+		return true
+	}
+
+	conf := conf(maxSize, 3)
+	if err := quick.Check(f, &conf); err != nil {
+		t.Fatal(err)
+	}
+}
