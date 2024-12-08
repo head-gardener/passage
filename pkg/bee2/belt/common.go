afc3cc9ff151e9c1418d4a6e86e5beca27f79007
Head Gardener <trashbin2019np@gmail.com>
2024-12-08 22:08:37 +0300

    refactor: errorMessage moved

--- pkg/bee2/belt/common.go
+++ pkg/bee2/belt/common.go
@@ -1,5 +1,30 @@
 package belt
 
+// #cgo LDFLAGS: -lbee2
+// #include <stdlib.h>
+// #include <bee2/core/err.h>
+import "C"
+
+import (
+	"errors"
+	"fmt"
+	"unsafe"
+)
+
+func errorMessage(code C.err_t) error {
+	if code == 0 {
+		return nil
+	}
+
+	ptr := C.errMsg((C.err_t)(code))
+	if uintptr(unsafe.Pointer(ptr)) == 0 {
+		return fmt.Errorf("unknown error %d", uintptr(unsafe.Pointer(ptr)))
+	}
+
+	msg := C.GoString(ptr)
+	return errors.New(msg)
+}
+
 // Produced by KDF, consumed by everything else. Same key shouldn't be used in different
 // algorithms. A 32 byte, 256 bit slice.
 type Key [32]byte
